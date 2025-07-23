d901eb854d963da70b4e824bf1e9fa28432273f1
kou <forte0801@gmail.com>
2025-07-23 09:25:28 +0900

    Add favicon and change links

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,3 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "川崎浩史 オフィシャルサイト";
+export const SITE_TWITTER = "https://x.com/bianchiidentity";
+export const SITE_GITHUB = "https://github.com/bianchiidentity";
